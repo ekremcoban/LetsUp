2b0835ad64a3aaf0e1673f7d998b9ac99717e0d6
Resul Ekrem Çoban <resulekremcoban@gmail.com>
2021-08-16 20:32:51 +0300

    trash

--- src/screens/activity-list/activity-list.screen.tsx
+++ src/screens/activity-list/activity-list.screen.tsx
@@ -67,12 +67,14 @@ export const ActivityListScreen = () => {
  
      const unsubscribe = navigation.addListener('focus', () => {
       console.log('İçerde');
-      analytics().logEvent('ActivityListScreen', {
-        id: 3745092,
-        item: 'mens grey t-shirt',
-        description: ['round neck', 'long sleeved'],
-        size: 'L',
-      })
+      analytics().logEvent('ActivityListScreen'
+      // , {
+      //   id: 3745092,
+      //   item: 'mens grey t-shirt',
+      //   description: ['round neck', 'long sleeved'],
+      //   size: 'L',
+      // }
+      )
      });
 
      preparingData(location);
