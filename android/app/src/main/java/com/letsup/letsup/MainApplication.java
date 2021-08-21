c69a32f6be003f1028fcaa030ecede4950d829e1
Resul Ekrem Çoban <resulekremcoban@gmail.com>
2021-08-22 01:35:14 +0300

    vs 0.27.8 --> Android gradle hatası cozuldu

--- android/app/src/main/java/com/letsup/letsup/MainApplication.java
+++ android/app/src/main/java/com/letsup/letsup/MainApplication.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
+import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
